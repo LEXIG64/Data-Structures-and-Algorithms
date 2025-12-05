3e5d297022c114243b7f8ed8c42b3bf418641a77
Lexi <agreenberg2@olin.edu>
2025-12-05 10:40:47 -0500

    Adding predictText

--- FinalProject/src/main/kotlin/Main.kt
+++ FinalProject/src/main/kotlin/Main.kt
@@ -1,33 +1,63 @@
 package org.example
 
+
+import java.io.File
 import kotlin.text.iterator
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val text = Trie(listOf("apple", "bears", "crown"))
-    println(text.search("apple") )
-    text.insert("deer")
-    println(text.search("deer"))
-    text.insert("deep")
-    println(text.search("deep"))
-    println(text.startRoot.children.last().childletters)
+
+    val wordFrequency = File("unigram_freq.csv")
+    val frequencyDict = mutableListOf<Pair<String, Int>>()
+    wordFrequency.forEachLine { line ->
+        val column = line.split(",")
+        frequencyDict.add(Pair(column[0], column[1].toInt()))
+    }
+
+    val dict = File("words_alpha.txt")
+    val correctDictionary = mutableSetOf<String>()
+    dict.forEachLine { line ->
+        correctDictionary.add(line)
+    }
+
+    val english = mutableListOf<String>()
+    for (d in frequencyDict) {
+        english.add(d.first)
+        if (d.first !in correctDictionary) {
+            frequencyDict.remove(d)
+        }
+    }
+
+    val englishTrie = Trie(frequencyDict, english)
+    val englishTrieRoot = englishTrie.startRoot
+    println(englishTrieRoot.childLetters)
 }
 
+/**
+ * Sets up the necessary characteristics of a Trie node
+ */
 class TrieNode(var letter: String,
                var progress: String,
                var end: Boolean,
                var parent: TrieNode?,
-               var childletters: MutableList<Char>,
-               var children: MutableList<TrieNode>) {}
+               var childLetters: MutableList<Char>,
+               var children: MutableList<TrieNode>,
+               var weight: Int = 0)
 
+/**
+ *
+ */
+class Trie (english: MutableList<Pair<String, Int>>, trieWords: MutableList<String>) {
 
-class Trie (english: List<String>) {
-
-    var trieSize = english.size
+    var wordsInTrie = trieWords
     val startRoot = build(english)
 
-    private fun build(words: List<String>): TrieNode {
+    /**
+     * Builds a trie from a list of strings representing real words
+     * @return the root node of the Trie that holds links to all its children and the rest of the nodes
+     */
+    private fun build(words: MutableList<Pair<String, Int>>): TrieNode {
         val root = TrieNode(
              "",
             "",
@@ -37,16 +67,20 @@ class Trie (english: List<String>) {
             mutableListOf())
         var holder: TrieNode
         var segment = ""
+        var count = 0
 
-        for (word in words) {
+        for (wordPair in words) {
+            val word = wordPair.first
+            val nodeWeights = wordPair.second/word.length
+            count += 1
 
             holder = root
 
             var continueCheck = 0
             for (str in word) {
-                if (holder.childletters.contains(word[continueCheck]) && segment == holder.progress) {
+                if (holder.childLetters.contains(word[continueCheck]) && segment == holder.progress) {
                     segment += str
-                    val index = holder.childletters.indexOf(str)
+                    val index = holder.childLetters.indexOf(str)
                     holder = holder.children[index]
                     continueCheck += 1
 
@@ -57,17 +91,19 @@ class Trie (english: List<String>) {
                             false,
                             holder,
                             mutableListOf(),
-                            mutableListOf()
+                            mutableListOf(),
+                            nodeWeights
                         )
 
-                        holder.children.add(node)
-                        holder.childletters.add(str)
-                        holder = node
+                    holder.children.add(node)
+                    holder.childLetters.add(str)
+                    holder = node
 
                     }
                 }
 
             holder.end = true
+            holder.weight = wordPair.second
 
             }
 
@@ -75,14 +111,17 @@ class Trie (english: List<String>) {
 
         }
 
-    fun insert(word: String) {
+    /**
+     *
+     */
+    fun insert(word: String, frequency: Int) {
         var holder = startRoot
         var segment = ""
         for (str in word) {
 
-            if (segment == holder.progress && holder.childletters.contains(str) ) {
+            if (segment == holder.progress && holder.childLetters.contains(str) ) {
                 segment += str
-                val index = holder.childletters.indexOf(str)
+                val index = holder.childLetters.indexOf(str)
                 holder = holder.children[index]
 
 
@@ -98,7 +137,7 @@ class Trie (english: List<String>) {
                 )
 
                 holder.children.add(node)
-                holder.childletters.add(str)
+                holder.childLetters.add(str)
                 holder = node
 
             }
@@ -106,21 +145,25 @@ class Trie (english: List<String>) {
         }
 
         holder.end = true
-        trieSize += 1
+        holder.weight = frequency
+        wordsInTrie.add(word)
     }
 
+    /**
+     *
+     */
     fun search(word: String): Boolean {
 
         var index = 0
         var holder = startRoot
 
-        while (holder.childletters.contains(word[index])) {
+        while (holder.childLetters.contains(word[index])) {
 
             if (holder.end && holder.progress == word) {
                 return true
             }
 
-            val childIndex = holder.childletters.indexOf(word[index])
+            val childIndex = holder.childLetters.indexOf(word[index])
             holder = holder.children[childIndex]
 
             if (index+1 == word.length) {
@@ -134,20 +177,115 @@ class Trie (english: List<String>) {
         return holder.end && holder.progress == word
     }
 
+    /**
+     *
+     */
+    fun locate(word: String): TrieNode? {
+
+        var index = 0
+        var holder = startRoot
+
+        while (holder.childLetters.contains(word[index])) {
+
+            if (holder.progress == word) {
+                return holder
+            }
+
+            val childIndex = holder.childLetters.indexOf(word[index])
+            holder = holder.children[childIndex]
+
+            if (index+1 == word.length) {
+                break
+            } else {
+                index += 1
+            }
+
+        }
+
+        if (holder.progress == word) {
+            return holder
+        }
+
+        return null
+    }
+
+    /**
+     *
+     */
     fun makeMap(): MutableMap<String, TrieNode> {
 
         val wordToNodes = mutableMapOf<String, TrieNode>()
 
-        while (wordToNodes.count() != trieSize) {
+        for (w in wordsInTrie) {
+            when (val temp = locate(w)) {
+                // Checks whether word is in true
+                null -> continue
 
-
+                // Default statement
+                else -> wordToNodes[w] = temp
+            }
         }
 
         return wordToNodes
     }
 }
 
+fun mostLikelyNext (trieNode: TrieNode): TrieNode {
+
+    var bestPath = trieNode
+    var greatestWeight = 0
+
+    for (child in trieNode.children) {
+        if (child.weight > greatestWeight) {
+            greatestWeight = child.weight
+            bestPath = child
+        }
+    }
+
+    return bestPath
+}
 
 
+fun predictText (segment: String, trie: Trie): Pair<String, String> {
+
+    val segmentNode = trie.locate(segment)
+    var possibilities = mutableListOf<TrieNode>()
+
+    for (child in segmentNode?.children!!) {
+        var currNode = child
+
+        while (!currNode.end) {
+            currNode = mostLikelyNext(currNode)
+        }
+
+        possibilities.add(currNode)
+    }
+
+    while (possibilities.size > 2) {
+        if (possibilities[0].weight > possibilities[1].weight) {
+            possibilities.removeAt(1)
+        }
+
+        if (possibilities[0].weight <= possibilities[1].weight) {
+            possibilities.removeAt(0)
+        }
+    }
+
+    return Pair(possibilities[0].progress, possibilities[1].progress)
+}
 
 
+fun spellCheck (text: String, trie: Trie): String {
+    val textBreakup = text.split(" ")
+    var fixedText = ""
+
+    for (phrase in textBreakup) {
+        if (trie.search(phrase)) {
+            fixedText = "$fixedText$phrase "
+        } else {
+
+        }
+    }
+
+    return fixedText
+}
